9d5b0b3dce1052d1fc243d8768ef403bfc4fb2e4
patrickgrey <websemantic@yahoo.co.uk>
2022-11-02 16:31:16 +0000

    Toggle button working

--- website-source/design-01/main.js
+++ website-source/design-01/main.js
@@ -1,5 +1,15 @@
 let hasReminders = true;
 
+const svg1 = document.querySelector(".ecrm-container h2 svg:nth-child(1)");
+const svg2 = document.querySelector(".ecrm-container h2 svg:nth-child(2)");
+const ecReminders = document.querySelector("#ecReminders");
+const ecNoReminders = document.querySelector("#ecNoReminders");
+
+
 document.querySelector("#ecAddReminders").addEventListener("click", function (event) {
-  console.log("ho")
+  hasReminders = !hasReminders;
+  svg1.style.display = hasReminders ? "inline-block" : "none";
+  svg2.style.display = !hasReminders ? "inline-block" : "none";
+  ecReminders.style.display = hasReminders ? "block" : "none";
+  ecNoReminders.style.display = !hasReminders ? "block" : "none";
 })
