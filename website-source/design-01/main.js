3058884d7ad2fa7164fd93cd4438d161b369a187
patrickgrey <websemantic@yahoo.co.uk>
2022-11-02 16:42:55 +0000

    Changed namespace to LZO

--- website-source/design-01/main.js
+++ website-source/design-01/main.js
@@ -1,12 +1,12 @@
 let hasReminders = true;
 
-const svg1 = document.querySelector(".ecrm-container h2 svg:nth-child(1)");
-const svg2 = document.querySelector(".ecrm-container h2 svg:nth-child(2)");
-const ecReminders = document.querySelector("#ecReminders");
-const ecNoReminders = document.querySelector("#ecNoReminders");
+const svg1 = document.querySelector(".lzo-container h2 svg:nth-child(1)");
+const svg2 = document.querySelector(".lzo-container h2 svg:nth-child(2)");
+const ecReminders = document.querySelector("#lzoReminders");
+const ecNoReminders = document.querySelector("#lzoNoReminders");
 
 
-document.querySelector("#ecAddReminders").addEventListener("click", function (event) {
+document.querySelector("#lzoAddReminders").addEventListener("click", function (event) {
   hasReminders = !hasReminders;
   svg1.style.display = hasReminders ? "inline-block" : "none";
   svg2.style.display = !hasReminders ? "inline-block" : "none";
