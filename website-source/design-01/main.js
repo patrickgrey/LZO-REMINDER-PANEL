e9aa4b07da48a361e659ba897aa3ebd61f3f216f
patrickgrey <websemantic@yahoo.co.uk>
2022-11-07 11:58:38 +0000

    Data loading done

--- website-source/design-01/main.js
+++ website-source/design-01/main.js
@@ -2,8 +2,8 @@
 
   const svg1 = document.querySelector(".lzo-panel-reminder h2 svg:nth-child(1)");
   const svg2 = document.querySelector(".lzo-panel-reminder h2 svg:nth-child(2)");
-  const ecReminders = document.querySelector("#lzoReminders");
-  const ecNoReminders = document.querySelector("#lzoNoReminders");
+  const lzoReminders = document.querySelector("#lzoReminders");
+  const lzoNoReminders = document.querySelector("#lzoNoReminders");
   const isDev = document.body.dataset.isDev;
 
   function convertRemToPixels(rem) {
@@ -24,7 +24,6 @@
     document.querySelectorAll("#lzoReminders > li").forEach(li => {
       const title = li.querySelector("details, h3");
       const buttons = li.querySelector(".lzo-bottom-container");
-      console.log(convertRemToPixels(10));
       if ((title.clientWidth + buttons.clientWidth + convertRemToPixels(7) < li.clientWidth) && window.innerWidth > 800) {
         buttons.style.position = "absolute";
         buttons.style.top = "1rem";
@@ -49,31 +48,102 @@
 
   window.addEventListener('resize', handleResize);
 
-  document.querySelectorAll(".lzo-button-guidance").forEach(button => {
-    button.addEventListener("click", function (event) {
-      const li = button.closest("li");
-      const guidance = li.querySelector(".lzo-guidance");
-      const isBlock = guidance.style.display === "block"
-      guidance.style.display = isBlock ? "none" : "block";
-    });
-  });
+
 
   function initUI(data) {
-    console.log(data);
     if (data.length === 0) {
-      ecNoReminders.style.display = document.querySelector(".lzo-panel-reminder h2 svg:nth-child(2)").style.display = "inline-block";
+      lzoNoReminders.style.display = document.querySelector(".lzo-panel-reminder h2 svg:nth-child(2)").style.display = "inline-block";
       return;
     }
     document.querySelector(".lzo-panel-reminder h2 svg:nth-child(1)").style.display = "inline-block";
 
+    const lzoCloneDaddy = document.querySelector("#lzoCloneDaddy");
+
+
     for (let index = 0; index < data.length; index++) {
-      // const liObject = data[index];
+      const liObject = data[index];
       // const li = document.createElement("li");
-      // ecReminders.appendChild(li);
+      // lzoReminders.appendChild(li);
 
       // Clone a hidden object like MUAC CV
-    }
+      const clone = lzoCloneDaddy.querySelector("li").cloneNode(true);
+      // clone.querySelector("h3 > span").textContent = liObject.task_items.length > 0 ? liObject.task_items.length.toString() : "1";
+      const h3 = clone.querySelector("h3");
+      const taskContainer = clone.querySelector(".lzo-task-container");
+      const details = clone.querySelector("details");
+      const summary = clone.querySelector("summary");
+      const taskList = clone.querySelector("details > ul");
+      const span = clone.querySelector("h3 > span");
+      const deadline = clone.querySelector(".lzo-deadline");
+      const buttonGuidance = clone.querySelector(".lzo-button-guidance");
+      const guidance = clone.querySelector(".lzo-guidance");
+      const buttonHelp = clone.querySelector(".lzo-button-help");
+      const buttonTask = clone.querySelector(".lzo-button-tasks");
 
+
+      if (liObject.task_priority != "") {
+        taskContainer.classList.add(`lzo-priority-${liObject.task_priority}`);
+      }
+
+      if (liObject.task_items.length > 0) {
+        span.textContent = liObject.task_items.length.toString();
+        summary.appendChild(h3);
+        for (let indexDetail = 0; indexDetail < liObject.task_items.length; indexDetail++) {
+          const li = document.createElement("li");
+          li.textContent = liObject.task_items[indexDetail].item_title;
+          taskList.appendChild(li);
+        }
+      }
+      else {
+        span.textContent = "1";
+        details.remove();
+      }
+
+      const task_title = document.createTextNode(liObject.task_title);
+      h3.appendChild(task_title);
+      lzoReminders.appendChild(clone);
+
+      if (liObject.task_deadline != "") {
+        const task_deadline = document.createTextNode(liObject.task_deadline);
+        deadline.appendChild(task_deadline);
+      }
+      else {
+        deadline.remove();
+      }
+
+      if (liObject.task_guidance != "") {
+        guidance.textContent = liObject.task_guidance;
+      }
+      else {
+        buttonGuidance.remove();
+        guidance.remove();
+      }
+
+      if (liObject.task_help_link != "") {
+        buttonHelp.href = liObject.task_help_link;
+      }
+      else {
+        buttonHelp.remove();
+      }
+
+      if (liObject.task_link != "") {
+        buttonTask.href = liObject.task_link;
+      }
+      else {
+        buttonTask.remove();
+      }
+
+    }
+    positionButtons();
+
+    document.querySelectorAll(".lzo-button-guidance").forEach(button => {
+      button.addEventListener("click", function (event) {
+        const li = button.closest("li");
+        const guidance = li.querySelector(".lzo-guidance");
+        const isBlock = guidance.style.display === "block"
+        guidance.style.display = isBlock ? "none" : "block";
+      });
+    });
   }
 
   async function loadData(person_ID) {
@@ -93,7 +163,6 @@
         initUI(responseData);
       }
       else {
-        console.log(response);
         errorLabel.textContent = "Error: " + (responseData && responseData.message) || response.status;
         errorLabel.style.display = "block";
       }
@@ -112,8 +181,8 @@
     hasReminders = !hasReminders;
     svg1.style.display = hasReminders ? "inline-block" : "none";
     svg2.style.display = !hasReminders ? "inline-block" : "none";
-    ecReminders.style.display = hasReminders ? "block" : "none";
-    ecNoReminders.style.display = !hasReminders ? "block" : "none";
+    lzoReminders.style.display = hasReminders ? "block" : "none";
+    lzoNoReminders.style.display = !hasReminders ? "block" : "none";
   });
 
   document.querySelector("#lzoToggleStylesheet")?.addEventListener("click", function (event) {
